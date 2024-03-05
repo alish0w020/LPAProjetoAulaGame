6cbfabed73326df56eadfa3dd462b94b2cbcddfc
alisson pereira <ali.pereira.oliveira@gmail.com>
2024-03-05 18:22:18 -0300

    Signed-off-by: alisson pereira <ali.pereira.oliveira@gmail.com>

---
+++ DiagramaClasseJogo/Level.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.entety_list = None
+        self.name = None
+
+    def run(self, ):
+        pass
+
